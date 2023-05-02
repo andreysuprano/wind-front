856f37cf9659c61674092b535d4e4b35459fc274
Andre Dias <andre.dias@alexandria.solar>
2023-05-01 21:44:00 -0300

    inputs com controller, faltando funcionar

--- src/components/ModalPerfil/index.tsx
+++ src/components/ModalPerfil/index.tsx
@@ -2,30 +2,32 @@ import { IUser } from "@/contexts/authContext";
 import { updateUsuario } from "@/services/api";
 
 import {
-	Input,
-	Modal,
-	ModalBody,
-	ModalCloseButton,
-	ModalOverlay,
-	ModalContent,
-	Button,
-	Stack,
-	FormControl,
-	FormLabel,
-	Avatar,
-	Heading,
-	AvatarBadge,
-	IconButton,
-	Flex,
-	useToast,
-	Text,
-	Progress,
-	Link
-} from '@chakra-ui/react';
-import { useEffect, useRef, useState } from 'react';
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
-import { storage } from '../../firebase/config';
-import { FaPencilAlt } from 'react-icons/fa';
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalOverlay,
+  ModalContent,
+  Button,
+  Stack,
+  FormControl,
+  FormLabel,
+  Avatar,
+  Heading,
+  AvatarBadge,
+  IconButton,
+  Flex,
+  useToast,
+  Text,
+  Progress,
+  Link,
+} from "@chakra-ui/react";
+import { useEffect, useRef, useState } from "react";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { storage } from "../../firebase/config";
+import { FaPencilAlt } from "react-icons/fa";
+import { Controller, SubmitHandler, useForm } from "react-hook-form";
+import { client } from "@/services/client";
 
 type ModalPerfil = {
   onClose: () => void;
@@ -37,130 +39,249 @@ type ModalPerfil = {
 type UserypePost = {
   avatar: string;
   name: string;
-  email: string;
-  senha: string;
+  username: string;
 };
 
 export const ModalPerfil = ({ onOpen, isOpen, onClose, user }: ModalPerfil) => {
-	const [imageFile, setImageFile] = useState<File>();
-	const [downloadURL, setDownloadURL] = useState('')
-	const [isUploading, setIsUploading] = useState(false)
-	const [progressUpload, setProgressUpload] = useState(0)
+  const {
+    formState: { errors },
+    control,
+    handleSubmit,
+    setValue,
+    watch,
+  } = useForm<UserypePost>({
+    defaultValues: {
+      avatar: "",
+      name: "",
+      username: "",
+    },
+  });
 
-	const toast = useToast();
-	const inputFile = useRef<any>();
-	
-	useEffect(() => {
-		if(imageFile){
-			handleUploadFile();
-		}
-	}, [imageFile]);
+  const [imageFile, setImageFile] = useState<File>();
+  const [downloadURL, setDownloadURL] = useState("");
+  const [isUploading, setIsUploading] = useState(false);
+  const [progressUpload, setProgressUpload] = useState(0);
 
-	const handleChangeAvatar = () => {
-		inputFile.current.click();
-	}
+  const toast = useToast();
+  const inputFile = useRef<any>();
+  const [loading, setLoading] = useState(true);
 
-	const handleSelectedFile = (files: any) => {
-		if (files && files[0].size < 10000000) {
-		  setImageFile(files[0])
-		  console.log(files[0])
-		} else {
-			toast({
-				title: `Foto muito pesada, selecione outra!`,
-				status: "warning",
-				isClosable: true,
-			});
-		}
-	  }
+  useEffect(() => {
+    if (user) {
+      setValue("avatar", user.avatar);
+      setValue("name", user.name);
+      setValue("username", user.username);
+    } else {
+      setValue("avatar", "");
+      setValue("name", "");
+      setValue("username", "");
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [user, setValue]);
 
-	const handleUploadFile = () => {
-		if (imageFile) {
-			const name = user?.sub;
-			const storageRef = ref(storage, `windfall/${name}`)
-			const uploadTask = uploadBytesResumable(storageRef, imageFile)
-			uploadTask.on(
-			'state_changed',
-			(snapshot) => {
-				const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-				setProgressUpload(progress) // to show progress upload
-				switch (snapshot.state) {
-				case 'paused':
-					console.log('Upload is paused')
-					break
-				case 'running':
-					console.log('Upload is running')
-					break
-				}
-			},
-			(error) => {
-				toast({
-					title: error + "",
-					status: "warning",
-					isClosable: true,
-				});
-			},
-			() => {
-					getDownloadURL(uploadTask.snapshot.ref).then((url) => {
-					setDownloadURL(url)
-				})
-			},
-			)
-		} else {
-			toast({
-				title: "Arquivo não encontrado!",
-				status: "warning",
-				isClosable: true,
-			});
-		}
-	}
+  console.log(user);
 
-	return (
-		<Modal onClose={onClose} isOpen={isOpen} isCentered>
-			<ModalOverlay />
-			<ModalContent>
-				<ModalCloseButton />
-				<ModalBody>
-					<Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }} marginBottom={'20px'}>
-						Meu Perfil
-					</Heading>
-					<FormControl id="userName">
-						<Progress colorScheme='green' size='sm' value={progressUpload} marginTop={'10px'}marginBottom={'10px'}/>
-						<FormLabel>Avatar</FormLabel>
-						<Flex marginBottom={'20px'}>
-							<Avatar size="xl" src={downloadURL ? downloadURL : user?.avatar}>
-								<AvatarBadge
-									as={IconButton}
-									size="sm"
-									rounded="full"
-									top="-10px"
-									colorScheme="red"
-									aria-label="remove Image"
-									icon={<FaPencilAlt />}
-									onClick={handleChangeAvatar}
-								/>
-							</Avatar>
-						<Input 
-							display="none" 
-							type='file' 
-							ref={inputFile} 
-							onChange={(files) => handleSelectedFile(files.target.files)}
-							accept="image/*"
-						/>
-						</Flex>
-					</FormControl>
-					<FormControl id="userName" isRequired>
-						<FormLabel>Nome</FormLabel>
-						<Input placeholder="Nome" _placeholder={{ color: 'gray.500' }} type="text" value={user?.name.split(' ')[0]}/>
-					</FormControl>
-					<FormControl id="lastName" isRequired>
-						<FormLabel>Sobrenome</FormLabel>
-						<Input placeholder="Sobrenome" _placeholder={{ color: 'gray.500' }} type="text" value={user?.name.split(' ')[1]}/>
-					</FormControl>
-					<FormControl id="email" isRequired marginBottom="10px">
-						<FormLabel>Email address</FormLabel>
-						<Input placeholder="your-email@example.com" _placeholder={{ color: 'gray.500' }} type="email" value={user?.username}/>
-					</FormControl>
-					{/* <Flex gap={'15px'}>
+  const handleUpdateUsuario: SubmitHandler<any> = (formData) => {
+    setLoading(true);
+    updateUsuario(formData)
+      .then(() => {
+        onClose();
+        // setEditingUserbyEmail("");
+        setLoading(false);
+        toast({
+          title: `Usuário editado com sucesso.`,
+          status: "success",
+          isClosable: true,
+        });
+      })
+      .catch((error) => {
+        setLoading(false);
+        if (!error.response) return;
+        toast({
+          title: `Não foi possível editar usuário.`,
+          status: "error",
+          isClosable: true,
+        });
+      });
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      handleUploadFile();
+    }
+  }, [imageFile]);
+
+  const handleChangeAvatar = () => {
+    inputFile.current.click();
+  };
+
+  const handleSelectedFile = (files: any) => {
+    if (files && files[0].size < 10000000) {
+      setImageFile(files[0]);
+      console.log(files[0]);
+    } else {
+      toast({
+        title: `Foto muito pesada, selecione outra!`,
+        status: "warning",
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleUploadFile = () => {
+    if (imageFile) {
+      const name = user?.sub;
+      const storageRef = ref(storage, `windfall/${name}`);
+      const uploadTask = uploadBytesResumable(storageRef, imageFile);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          setProgressUpload(progress); // to show progress upload
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+          }
+        },
+        (error) => {
+          toast({
+            title: error + "",
+            status: "warning",
+            isClosable: true,
+          });
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+            setDownloadURL(url);
+          });
+        }
+      );
+    } else {
+      toast({
+        title: "Arquivo não encontrado!",
+        status: "warning",
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Modal onClose={onClose} isOpen={isOpen} isCentered>
+      <ModalOverlay />
+      <form onSubmit={handleSubmit(handleUpdateUsuario)}>
+        <ModalContent>
+          <ModalCloseButton />
+          <ModalBody>
+            <Heading
+              lineHeight={1.1}
+              fontSize={{ base: "2xl", sm: "3xl" }}
+              marginBottom={"20px"}
+            >
+              Meu Perfil
+            </Heading>
+            <Controller
+              name="avatar"
+              control={control}
+              render={({ field: { onChange, value } }) => (
+                <FormControl id="userName">
+                  <Progress
+                    colorScheme="green"
+                    size="sm"
+                    value={progressUpload}
+                    marginTop={"10px"}
+                    marginBottom={"10px"}
+                  />
+                  <FormLabel>Avatar</FormLabel>
+                  <Flex marginBottom={"20px"}>
+                    <Avatar
+                      size="xl"
+                      src={downloadURL ? downloadURL : user && user.avatar}
+                    >
+                      <AvatarBadge
+                        as={IconButton}
+                        size="sm"
+                        rounded="full"
+                        top="-10px"
+                        colorScheme="red"
+                        aria-label="remove Image"
+                        icon={<FaPencilAlt />}
+                        onClick={handleChangeAvatar}
+                      />
+                    </Avatar>
+                    <Input
+                      display="none"
+                      type="file"
+                      ref={inputFile}
+                      onChange={(files) =>
+                        handleSelectedFile(files.target.files)
+                      }
+                      accept="image/*"
+                    />
+                  </Flex>
+                </FormControl>
+              )}
+              rules={{ required: true }}
+            />
+
+            <FormControl id="userName" isRequired>
+              <FormLabel>Nome</FormLabel>
+              <Controller
+                name="name"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Input
+                    placeholder="Nome"
+                    _placeholder={{ color: "gray.500" }}
+                    type="text"
+                    {...field}
+                  />
+                )}
+              />
+              {/* <Input
+                placeholder="Nome"
+                _placeholder={{ color: "gray.500" }}
+                type="text"
+                value={user?.name.split(" ")[0]}
+              /> */}
+            </FormControl>
+            {/* <FormControl id="lastName" isRequired>
+              <FormLabel>Sobrenome</FormLabel>
+              <Input
+                placeholder="Sobrenome"
+                _placeholder={{ color: "gray.500" }}
+                type="text"
+                value={user?.name.split(" ")[1]}
+              />
+            </FormControl> */}
+            <FormControl id="email" isRequired marginBottom="10px">
+              <FormLabel>Email address</FormLabel>
+              <Controller
+                name="username"
+                control={control}
+                rules={{ required: true }}
+                render={({ field }) => (
+                  <Input
+                    placeholder="your-email@example.com"
+                    _placeholder={{ color: "gray.500" }}
+                    type="email"
+                    {...field}
+                  />
+                )}
+              />
+              {/* <Input
+                placeholder="your-email@example.com"
+                _placeholder={{ color: "gray.500" }}
+                type="email"
+                value={user?.username}
+              /> */}
+            </FormControl>
+            {/* <Flex gap={'15px'}>
 						<FormControl id="password" isRequired>
 							<FormLabel>Senha</FormLabel>
 							<Input placeholder="*******" _placeholder={{ color: 'gray.500' }} type="password" />
@@ -170,33 +291,44 @@ export const ModalPerfil = ({ onOpen, isOpen, onClose, user }: ModalPerfil) => {
 							<Input placeholder="*******" _placeholder={{ color: 'gray.500' }} type="password" />
 						</FormControl>
 					</Flex> */}
-					<Link href="#">Trocar minha senha.</Link>
-					<Stack spacing={6} direction={[ 'column', 'row' ]} marginTop={'20px'} marginBottom={'20px'}>
-						<Button
-							bg={'red.400'}
-							color={'white'}
-							w="full"
-							_hover={{
-								bg: 'red.500'
-							}}
-							disabled={isUploading}
-						>
-							Cancelar
-						</Button>
-						<Button
-							bg={'blue.400'}
-							color={'white'}
-							w="full"
-							_hover={{
-								bg: 'blue.500'
-							}}
-							disabled={isUploading}
-						>
-							Salvar
-						</Button>
-					</Stack>
-				</ModalBody>
-			</ModalContent>
-		</Modal>
-	)
+            <Link href="/forgot-password">Trocar minha senha.</Link>
+            <Stack
+              spacing={6}
+              direction={["column", "row"]}
+              marginTop={"20px"}
+              marginBottom={"20px"}
+            >
+              <Button
+                bg={"red.400"}
+                color={"white"}
+                w="full"
+                _hover={{
+                  bg: "red.500",
+                }}
+                disabled={isUploading}
+                onClick={() => {
+                  onClose();
+                  // setEditingUserbyEmail("");
+                }}
+              >
+                Cancelar
+              </Button>
+              <Button
+                bg={"blue.400"}
+                color={"white"}
+                w="full"
+                _hover={{
+                  bg: "blue.500",
+                }}
+                disabled={isUploading}
+                type="submit"
+              >
+                Salvar
+              </Button>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </form>
+    </Modal>
+  );
 };
